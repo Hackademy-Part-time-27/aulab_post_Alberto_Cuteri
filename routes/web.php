4495b480ebe0e4a61a21c6e154557be63dede42c
iretuc <albertocuteri88@gmail.com>
2024-05-14 21:21:03 +0200

    seconda importazione

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\PublicController;
+
+Route::get('/', [PublicController::class, 'homepage'])->name('homepage');
