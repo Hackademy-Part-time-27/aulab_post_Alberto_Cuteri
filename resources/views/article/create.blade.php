dcae5a89825d3318c4eac9621753260e23a169ca
iretuc <albertocuteri88@gmail.com>
2024-05-18 21:36:08 +0200

    continuazione articoli

---
+++ resources/views/article/create.blade.php
@@ -0,0 +1,61 @@
+<x-layout>
+    <div class="container-fluid p-5 bg-info text-center">
+        <div class="row justify-content-center">
+            <div class='col-12'>
+                <h1 class="display-1">Inserisci un articolo</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-8">
+                <form class="card p-5 shadow" action="{{route('article.store')}}" method="POST" enctype="multipart/form-data">    
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Titolo</label>
+                        <input type="text" name="title" class="form-control" id='title' value="{{ old('title')}}">
+                        @error('title')
+                        <span class="text-danger">{{$message}}</span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="subtitle" class="form-label">Sottotitolo:</label>
+                        <input type="text" name="subtitle" class="form-control" id='subtitle' value="{{ old('subtitle')}}">
+                        @error('subtitle')
+                            <span class="text-danger">{{$message}}</span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="image" class="form-label">Immagine</label>
+                        <input type="file" name="image" class="form-control" id='image'>
+                        @error('image')
+                            <span class="text-danger">{{$message}}</span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                    <label for="category" class="form-label">Categoria</label>
+                    <select name="category" class="form-control" id="category">
+                        <option selected disabled>Seleziona Categoria</option>
+                        @foreach ($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                    @error('image')
+                    <span class="text-danger">{{$message}}</span>
+                    @enderror
+                </div>
+                    <div class="mb-3">
+                        <label for="body" class="form-label">Testo</label>
+                        <textarea name="body" id="body" cols="30" rows="7" class="form-control">{{ old('body')}}</textarea>
+                        @error('body')
+                            <span class="text-danger">{{$message}}</span> 
+                        @enderror   
+                    </div>
+                    <div class="mt-3 d-flex justify-content-center flex-column align-items-center">
+                        <button type="submit" class="btn btn-outline-secondary">Inserisci Articolo</button>
+                        <a href="{{route('homepage')}}" class="text-secondary mt-2">Torna alla Home"</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PublicController;
+use App\Http\Controllers\ArticleController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\Controllers\PublicController;
+
 
 Route::get('/', [PublicController::class, 'homepage'])->name('homepage');
+Route::get('/article/create', [ArticleController::class, 'create'])->name('article.create');
