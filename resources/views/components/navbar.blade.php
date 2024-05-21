7f4db49a11d0b1648ef380796d0909dd26db22fa
iretuc <albertocuteri88@gmail.com>
2024-05-21 19:51:16 +0200

    Creato primo articolo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,14 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{route('homepage')}}">Home</a>
+                </li>
 @auth
 <li class="nav-item">
   <a class="nav-link" href="{{route('article.create')}}">Inserisci un articolo</a>
@@ -29,3 +40,11 @@
     </ul>
   </li>
 @endguest
+</ul>
+            <form action="#" class="d-flex" role="search">
+                <input class="form-control me-2" type="search"  aria-label="Search">
+                <button class="btn btn-outline-secondary" type="submit">Cerca</button>
+            </form>
+        </div>
+    </div>
+</nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div class="container-fluid p-5 bg-secondary-subtle text-center">
+    <div class="container-fluid p-5 bg-info-subtle text-center">
         <div class="row justify-content-center">
             <div class="col-12">
                 <h1 class="display-1">The Aulab Post</h1>
@@ -12,5 +12,27 @@
             {{ session('message') }}
         </div>
     @endif
-    
+    <div class="container mt-5">
+        <div class="row justify-content-between">
+            @foreach ($articles as $article)
+                <div class="col-12 col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ Storage::url($article->image) }}" class="card-img-top" alt="Immagine articolo: {{ $article->title }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $article->title }}</h5>
+                            <p class="card-subtitle">{{ $article->subtitle }}</p>
+                            <p class="small text-muted">Categoria:
+                                <a href="#" class="text-capitalize text-muted">{{ $article->category->name }}</a>
+                            </p>
+                        </div>
+                        <div class="card-footer d-flex justify-content-between align-items-center">
+                            <p>Redatto il {{$article->created_at->format('d/m/Y')}} <br>
+                    da {{$article->user->name}}</p>
+                            <a href="#" class="btn btn-outline-secondary">Leggi</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </x-layout>
