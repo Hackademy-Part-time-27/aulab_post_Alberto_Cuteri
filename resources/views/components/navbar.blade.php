53e689482761ee5067ff135d385ca6a8989a4b3f
iretuc <albertocuteri88@gmail.com>
2024-05-27 17:38:24 +0200

    User Story 3 inserite credenziali mail nel file env, riprendo da PublicController, careersSubmit

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -12,6 +12,9 @@
                 <li class="nav-item">
                   <a class="nav-link active" aria-current="page" href="{{ route('article.index') }}">Tutti gli articoli</a>
                 </li>
+                <li class="nav-item">
+          <a class="nav-link  linkCustom" aria-current="page" href="{{route('careers')}}">Lavora con noi</a>
+        </li>
 @auth
 <li class="nav-item">
   <a class="nav-link" href="{{route('article.create')}}">Inserisci un articolo</a>
